aa53683bb7ece099061ed401c020fe9c13a73d22
Becca <rbuechle@my365.bellevue.edu>
2021-01-13 19:13:33 -0700

    Updates

---
+++ server/routes/security-question-api.js
@@ -0,0 +1,9 @@
+// require statements
+const express = require('express');
+const SecurityQuestion = require('../db-models/security-question');
+
+// configurations
+const router = express.Router();
+
+
+//
