9d274b7eab0f74ca23488b66617506016af91fab
Becca <rbuechle@my365.bellevue.edu>
2021-01-18 23:38:34 -0700

    fix

--- src/app/shared/auth-layout/auth-layout.component.ts
+++ src/app/shared/auth-layout/auth-layout.component.ts
@@ -1,3 +1,4 @@
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,7 +10,7 @@ export class AuthLayoutComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
